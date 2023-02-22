50b19183bf358a9b31dad18c06a07ff30aff58c7
elishambadi <embadi43@gmail.com>
2023-02-22 17:28:34 +0300

    add annotations

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -4,5 +4,10 @@
 """
 
 
-def index_range(page, page_size):
+def index_range(page: int, page_size: int) -> tuple:
+    """
+    index_range() - returns an index range
+    Args: page, page_size
+    Returns: tuple
+    """
     return (page_size * (page - 1), page_size * page)
