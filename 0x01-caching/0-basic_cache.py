d78616fc7cf0b2053274fb32fd629c21cf01c171
elishambadi <embadi43@gmail.com>
2023-10-25 20:19:44 +0300

    init

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""Caching Module"""
+
+BaseCaching = __import__('base').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """Basic Cache Class"""
+    def put(self, key, item):
+        """Puts an item into the cache"""
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Gets an item from the cache"""
+        return self.cache_data.get(key, None)
---
+++ 0x01-caching/README.md
@@ -0,0 +1,7 @@
+# Cache Replacement Algorithms
+This repo explores the implementation of different Cache Replacement Algos.
+
+1.
+1.
+1.
+1. 
