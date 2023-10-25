f73f34cee07edd99e9d06cad35d0c397e53da36e
elishambadi <embadi43@gmail.com>
2023-10-25 19:51:39 +0300

    deletion resistant

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -52,7 +52,7 @@ class Server:
             return []
 
         return dataset[start:end]
-    
+
     def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
         """
             Return a HATEOAS optimized dics
