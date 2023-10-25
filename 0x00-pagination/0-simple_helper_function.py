6ec283d556a192061b46215b407d0a423c1c70fd
elishambadi <embadi43@gmail.com>
2023-10-25 19:14:55 +0300

    add readme

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,6 +2,11 @@
 """Pagination Functions"""
 
 def index_range(page, page_size):
+    """
+    Returns an index range of items for pagination.
+    Args: page: int, page_size: int
+    Returns: tuple
+    """
     if page <= 0 or page_size <= 0:
         return None
 
---
+++ 0x00-pagination/README.md
@@ -0,0 +1,5 @@
+#0x00 - Pagination
+
+Pagination Scripts for API Design
+1. Simple Pagination
+2. HATEOAS
