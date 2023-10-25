bb408482a5d184db9137a2a5b5fe1f4e6886ae1d
elishambadi <embadi43@gmail.com>
2023-10-25 19:10:52 +0300

    index range

---
+++ 0x00-pagination/0-simple_helper_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Pagination Functions"""
+
+def index_range(page, page_size):
+    if page <= 0 or page_size <= 0:
+        return None
+
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+
+    return start_index, end_index
